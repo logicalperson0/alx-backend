0db7dafb740a641c3f26c7a022090b65882ac9c8
Samuel <a2jsamuel@gmail.com>
2023-10-21 16:40:38 +0300

    fix for pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ Module with a single fun: Simple helper function
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """function returns a tuple of size two containing a start index and an
     end index corresponding to the range of indexes to return in a list
