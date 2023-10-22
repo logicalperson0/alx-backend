6b7606732824dd3fb3c0cd22d0fc848fed8713be
Samuel <a2jsamuel@gmail.com>
2023-10-22 16:37:15 +0300

    This is for task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Module with a single fun: index_range function and Server class
+"""
+import csv
+import math
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """function returns a tuple of size two containing a start index and an
+    end index corresponding to the range of indexes to return in a list
+    for those particular pagination parameters"""
+    start_index = (page - 1) * page_size
+    end_index = (start_index + page_size)
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Uses the index_range to find the correct indexes to paginate
+        the dataset correctly and return the appropriate page of the
+        dataset """
+        assert (page > 0 and type(page) is int)
+        assert (page_size > 0 and type(page_size is int))
+
+        tup_ind = index_range(page, page_size)
+
+        data_file = self.dataset()
+        try:
+            return data_file[tup_ind[0]:tup_ind[1]]
+        except IndexError:
+            return []
