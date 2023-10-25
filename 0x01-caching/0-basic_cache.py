99c4312b3728577bc23716a7ce412bb91f005a77
Samuel <a2jsamuel@gmail.com>
2023-10-25 18:12:58 +0300

    This is for task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 
+BaseCaching module Class that inherits from a parent class
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """class that inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """Init the class bacicache"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assigns to the dictionary self.cache_data the item
+        value for the key key"""
+        if key or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+    
+    def get(self, key):
+        """returns the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
