b1c09d86a4681d1f71698181139e4791842863be
Samuel <a2jsamuel@gmail.com>
2023-10-31 23:58:39 +0300

    fix 1

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -19,6 +19,14 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 @babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
@@ -33,14 +41,6 @@ def get_locale():
         return headers_lan
 
     return request.accept_languages.best_match(LANGUAGES)
-
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
 
 
 def get_user() -> Union[Dict, None]:
