436e302ab7dffbfb98661dd4cd163a6378ac9ab8
Samuel <a2jsamuel@gmail.com>
2023-11-01 00:39:30 +0300

    chk and fix

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -25,7 +25,7 @@ def get_locale():
     locale = request.args.get('locale')
     if locale in Config.LANGUAGES:
         return locale
-    return request.accept_languages.best_match(LANGUAGES)
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 users = {
