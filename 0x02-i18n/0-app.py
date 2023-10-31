e90c73de79ebfd2e9ae14e3aa22fc298e2a12c5b
Samuel <a2jsamuel@gmail.com>
2023-10-31 13:57:43 +0300

    fixs the render of index method on 0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    """renders an index.html template"""
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
