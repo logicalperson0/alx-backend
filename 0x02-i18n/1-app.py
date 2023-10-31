9c7eeabf2c30d4f6c4e5b01dfe6d3fe5f2a9a079
Samuel <a2jsamuel@gmail.com>
2023-10-31 14:09:38 +0300

    this is for task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+a basic Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """configure available languages in our app"""
+    BABEL_DEFAULT_LOCALE = 'en'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config = Config
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """renders an index.html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
