530a33fb14b95fd0499e5c043b18278963743d3d
Samuel <a2jsamuel@gmail.com>
2023-10-31 18:51:25 +0300

    this is for task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+a basic Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """configure available languages in our app"""
+    BABEL_DEFAULT_LOCALE = 'en'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(LANGUAGES)
+
+
+@app.route("/")
+def index():
+    """renders an index.html template"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
