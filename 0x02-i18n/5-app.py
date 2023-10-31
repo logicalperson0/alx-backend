b7b990debc308b2a15f65dd00ef2ab6019fbded5
Samuel <a2jsamuel@gmail.com>
2023-10-31 19:20:38 +0300

    fix and see for 5-file

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -26,12 +26,14 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(LANGUAGES)
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 def get_user(user_id):
     """returns a user dictionary or None if the ID cannot be
@@ -40,11 +42,13 @@ def get_user(user_id):
         return users.get(int(user_id))
     return None
 
+
 @app.before_request
 def before_request():
     """use get_user to find a user if any, and set
     it as a global on flask.g.user."""
     g.user = get_user(request.args.get('login_as'))
+
 
 @app.route("/")
 def index():
