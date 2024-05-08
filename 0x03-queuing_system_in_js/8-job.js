e5492d0adeb8463114658e07f93c652b1e6df786
Samuel <a2jsamuel@gmail.com>
2024-05-08 12:20:32 +0300

    tasks: 10 and 11

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,29 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw Error('Jobs is not an array');
+  }
+  jobs.forEach((x) => {
+    const job = queue.create('push_notification_code_3', x)
+      .save((err) => {
+        if (!err) console.log(`Notification job created: ${job.id}`);
+      });
+    
+      job.on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      });
+    
+      job.on('failed', (err) => {
+        console.log(`Notification job ${job.id} failed: ${err}`);
+      });
+    
+      job.on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+  });
+};
+
+module.exports = createPushNotificationsJobs;
